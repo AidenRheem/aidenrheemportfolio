9baa1af4229237943cdc39b7c939b23504a5fb10
roschreiber <schreiber.robin@icloud.com>
2025-04-03 21:04:06 +0200

    ship current main page layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import type { LedMatrixConfig } from '@fabwaseem/easy-led-matrix';
+  import "../app.css";
+
+  let { children } = $props();
+  let container: HTMLDivElement;
+  let matrix;
+  let LedMatrix;
+  let config: LedMatrixConfig = {
+    shape: 'square', 
+    size: 12,
+    color: '#161819',
+    amount: 1000,
+    gap: 2,
+    litColor: '#28282B',
+    fps: 24,
+    noise: 0.005,
+    background: '#000000',
+  };
+
+  onMount(async () => {
+    if (typeof window !== 'undefined') {
+      const mod = await import('@fabwaseem/easy-led-matrix');
+      LedMatrix = mod.LedMatrix;
+      matrix = new LedMatrix(container, config);
+      matrix.init();
+    }
+  });
+</script>
+
+<div class="relative min-h-screen">
+  <div bind:this={container} class="absolute inset-0 w-full h-full z-0"></div>
+  
+  <div class="min-h-screen relative z-10">
+    {@render children()}
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<div class="min-h-screen flex items-center justify-center">
+  <div class="container max-w-screen-sm border border-overlay2 p-24 relative bg-black/50 backdrop-blur-sm text-[#28282B]">
+    <div class="flex items-center justify-center pb-6">
+      <!-- my logo, top left -->
+      <div class="absolute top-2 left-6 flex items-center text-3xl">
+        <a class="moonhouse text-[#a6a6ad]" href="/">r</a>
+        <a class="moonhouse text-[#a6a6ad]" href="/" style="margin-left: -4.5px;">.</a> <!-- todo: connect dot to discord status through lanyard-->
+      </div>
+
+      <!-- hrefs, top right -->
+      <div class="absolute top-4.75 right-6 flex items-center">
+        <span class="krypton text-sm text-[#a6a6ad]" style="margin-left: -2px;">
+          <a href="/projects" class="underline text-[#a6a6ad]">projects</a>
+          <a href="/socials" class="underline text-[#a6a6ad]">socials</a>
+          <a href="/music" class="underline text-[#a6a6ad]">music</a>
+        </span>
+      </div>  
+    </div>
+  </div>
+</div>
