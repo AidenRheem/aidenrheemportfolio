4f301736003f1d28c20dd29d5cdca846300b6e52
roschreiber <schreiber.robin@icloud.com>
2025-04-05 19:43:42 +0200

    Add discord status dot!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,27 +11,45 @@
   } | null = null;
 
   const discordID = '316991053242564609'; // maybe we move this into env?
+  let statusData = '';
 
   function updateTime() {
     const d = new Date();
     const localTime = d.getTime();
     const localOffset = d.getTimezoneOffset() * 60000;
     const utc = localTime + localOffset;
-    const offset = 2; // Germany's timezone offset
+    const offset = 2; 
     const germany = utc + 3600000 * offset;
-    germanyTimeNow = new Date(germany).toLocaleTimeString(); // Only show the time
+    germanyTimeNow = new Date(germany).toLocaleTimeString();
   }
 
   const fetchSpotify = async () => {
     try {
       const response = await fetch(`https://api.lanyard.rest/v1/users/${discordID}`);
       const data = await response.json();
-      spotifyData = data.data.spotify; // Assign the fetched data to spotifyData
+      spotifyData = data.data.spotify;
+      statusData = data.data.discord_status;
+
     } catch (error) {
       console.error('Error fetching Spotify data:', error);
-      spotifyData = null; // Reset spotifyData in case of an error
+      spotifyData = null; 
     }
   };
+
+  let dotColor = "#a6a6ad";
+
+
+  $: {
+  if (statusData === "offline") {
+    dotColor = "#a6a6ad";
+  } else if (statusData === "dnd") {
+    dotColor = "#812e25";
+  } else if (statusData === "idle") {
+    dotColor = "#f79c18";
+  } else if (statusData === "online") {
+    dotColor = "#4f8832";
+  }
+}
 
   onMount(() => {
     updateTime();
@@ -45,16 +63,17 @@
       clearInterval(spotifyInterval);
     };
   });
+
+  console.log(dotColor, statusData, spotifyData);
 </script>
 
 <div class="min-h-screen flex items-center justify-center">
-  <!-- Reduced padding from p-24 to p-12 -->
   <div class="container max-w-screen-sm border border-overlay2 p-18 relative bg-black/50 backdrop-blur-sm text-[#28282B]">
     <div class="flex items-center justify-center pb-6">
       <!-- my logo, top left -->
       <div class="absolute top-2 left-6 flex items-center text-3xl group hover:text-[#dbdbde]">
         <a class="moonhouse text-[#a6a6ad] group-hover:text-[#dbdbde]" href="/">r</a>
-        <a class="moonhouse text-[#a6a6ad] group-hover:text-[#dbdbde]" href="/" style="margin-left: -4.5px;">.</a>
+        <a class="moonhouse group-hover:text-[#dbdbde]" href="/" style="margin-left: -4.5px; color: {dotColor};">.</a>
       </div>
 
       <!-- hrefs, top right -->
@@ -63,6 +82,7 @@
           <a href="/projects" class="underline text-[#a6a6ad] hover:text-[#dbdbde]">projects</a>
           <a href="/socials" class="underline text-[#a6a6ad] hover:text-[#dbdbde]">socials</a>
           <a href="/music" class="underline text-[#a6a6ad] hover:text-[#dbdbde]">music</a>
+          <a href="/blog" class="underline text-[#a6a6ad] hover:text-[#dbdbde]">blog</a>
         </span>
       </div>
 
@@ -80,10 +100,8 @@
       <div class="flex items-center">
         {#if spotifyData}
         <a id="spotify-status" href={`https://open.spotify.com/track/${spotifyData.track_id}`} target="_blank" class="flex items-center w-full">
-          <!-- Smaller image with padding -->
           <img src={spotifyData.album_art_url} alt={spotifyData.album} class="h-16 w-16 p-1">
 
-          <!-- Compact text layout -->
           <div class="flex-1 pl-2 overflow-hidden">
             <p class="text-base font-semibold text-[#dbdbde] krypton text-clip whitespace-nowrap overflow-hidden">{spotifyData.song}</p>
             <p class="text-xs text-gray-400 krypton truncate">{spotifyData.artist}</p>
